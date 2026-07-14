98d5703cb0540152053f4727b26002dd4c5f47dc
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-07-14 16:16:10 +0900

    Squareコンポーネントと3x3の盤面を追加

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
-export default function Square() {
-    return <button className="square">X</button>;
+function Square() {
+  return <button className="square"></button>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
