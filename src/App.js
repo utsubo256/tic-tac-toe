17b68c924907f70e5d3418dfd8dd1553c3fd3387
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-07-14 21:14:27 +0900

    勝者判定とゲームのステータス表示を追加

--- src/App.js
+++ src/App.js
@@ -12,8 +12,16 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const winner = culculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || culculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -28,6 +36,7 @@ export default function Board() {
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -45,4 +54,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function culculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
